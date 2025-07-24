6dfd17f423175d1660e6ec0cf848b3e80494e6c9
Eryüksel Aşkar <eryukselaskar@gmail.com>
2025-07-24 15:04:52 +0300

    feat: dynamic environment.ts for Vercel

---
+++ generate-env.ts
@@ -0,0 +1,20 @@
+// generate-env.ts
+const fs = require('fs');
+const path = './src/environments/environment.ts';
+
+const envContent = `
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: '${process.env.FIREBASE_API_KEY}',
+    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
+    projectId: '${process.env.FIREBASE_PROJECT_ID}',
+    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
+    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
+    appId: '${process.env.FIREBASE_APP_ID}'
+  }
+};
+`;
+
+fs.writeFileSync(path, envContent);
+console.log('✅ environment.ts dosyası oluşturuldu');
